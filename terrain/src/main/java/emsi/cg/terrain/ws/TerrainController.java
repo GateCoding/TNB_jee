b3a2723ad0b5a38007a8d469215abaedbfb5f754
GateCoding <meriammeri.mm@gmail.com>
2024-01-13 12:20:07 +0100

    ms terrain

---
+++ terrain/src/main/java/emsi/cg/terrain/ws/TerrainController.java
@@ -0,0 +1,34 @@
+package emsi.cg.terrain.ws;
+
+import com.example.tnb.entity.Terrain;
+import com.example.tnb.service.RedevableService;
+import com.example.tnb.service.TerrainService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api")
+public class TerrainController {
+
+    @Autowired
+    TerrainService ts;
+    @Autowired
+    RedevableService rs;
+
+    @GetMapping("/terrain")
+    public List<Terrain> getAllTerrain(){
+        return ts.findAll();
+    }
+
+    @PostMapping("/terrain/save")
+    public void saveTerrain(@RequestBody Terrain t){
+        ts.save(t);
+    }
+
+    @GetMapping("/terrain/redevable")
+    public List<Terrain> getTerrainByRevendevale(@RequestParam("cin") String cin){
+        return ts.findByRedevableCin(cin);
+    }
+}
