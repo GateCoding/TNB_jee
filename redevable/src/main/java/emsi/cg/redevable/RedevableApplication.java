b37da862ed0a4efc12cb26f3213e6f6650af192b
GateCoding <meriammeri.mm@gmail.com>
2024-01-13 13:24:56 +0100

    ms redevable

--- redevable/src/main/java/emsi/cg/redevable/RedevableApplication.java
+++ redevable/src/main/java/emsi/cg/redevable/RedevableApplication.java
@@ -2,8 +2,10 @@ package emsi.cg.redevable;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class RedevableApplication {
 
     public static void main(String[] args) {
---
+++ redevable/src/main/java/emsi/cg/redevable/service/TerrainMS.java
@@ -0,0 +1,35 @@
+package emsi.cg.redevable.service;
+
+import emsi.cg.redevable.entity.Categorie;
+import emsi.cg.redevable.entity.Redevable;
+import emsi.cg.redevable.entity.Taux;
+import emsi.cg.redevable.entity.Terrain;
+import org.springframework.cloud.openfeign.FeignClient;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+
+@FeignClient(name="SERVICE-TERRAIN")
+public interface TerrainMS {
+
+
+    @GetMapping(path = "/api/categorie/find/{id}")
+    Categorie categorieById (@PathVariable("id") Long id);
+
+    @GetMapping(path = "/api/taux/find/{id}")
+    Taux tauxById (@PathVariable("id") Long id);
+
+
+    @GetMapping(path = "/api/terrain/find/{id}")
+    Terrain terrainById (@PathVariable("id") Long id);
+
+
+
+    @GetMapping("/terrain/redevable")
+    List<Terrain> findByRedevableCin(String cin);
+
+
+}
